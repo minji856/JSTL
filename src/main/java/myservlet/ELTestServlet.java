efd4297a1dc48ea8344e643e88ed6fb9fc32c16c
MinJi Choi <sistar96@gmail.com>
2023-10-06 10:35:11 +0900

    배열EL

--- src/main/java/myservlet/ELTestServlet.java
+++ src/main/java/myservlet/ELTestServlet.java
@@ -2,6 +2,7 @@ package myservlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,6 +16,16 @@ public class ELTestServlet extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String name = req.getParameter("name");
+		
+		/* 방문자 수를 구했다 치고 */
+		req.setAttribute("visitCount", 100);
+		
+		String[] favorateFood = {"냉면" , "돼지갈비" , "순대국" , "콩국수"};
+		req.setAttribute("foods", favorateFood);
+		
+		/* 다른 애에게 역할을 맡기겠다 지금 받은 정보를 이 페이지에 넘겨줄 계획이다 */
+		RequestDispatcher view = req.getRequestDispatcher("test08_proc.jsp");
+		view.forward(req, resp);
 	}
-
 }
--- src/main/webapp/test08_proc.jsp
+++ src/main/webapp/test08_proc.jsp
@@ -6,6 +6,21 @@
 <title>test08_proc.jsp</title>
 </head>
 <body>
-
+	<!-- request 를 통해서 정보를 받을 수 있다 -->
+	<h2> <%= request.getParameter("name") %>님 환영합니다.</h2> <!--옛날방법. jsp니까 가능-->
+	<h2> ${param.name}님 환영합니다.</h2>
+	
+	총 방문자 수 : <%= request.getAttribute("visitCount") %> 명 <br>
+	<!-- requestScope 생략하면 page 부터 찾기 시작하지만 가급적 생략 지양 -->
+	총 방문자 수 : ${requestScope.visitCount} 명 <br>
+	<hr>
+	<h2>${param.name}님이 좋아하는 음식</h2>
+	<%
+		String[] foods = (String[])request.getAttribute("foods");
+	%>
+	
+	<%= foods[0] %>, <%= foods[1] %>, <%= foods[2] %>, <%= foods[3] %>
+	<br><br>
+	${foods}, ${requestScope.foods[0]}, ${foods[1]}, ${foods[2]}, ${foods[3]}
 </body>
 </html>
